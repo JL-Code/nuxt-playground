4ed93b310463988479adb9a2322c6ae7be1e0669
jiangy <jiangliu0316@outlook.com>
2025-02-05 13:07:51 +0800

    feat: 增加element plus affix poc

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nuxt-schema-org", "@nuxtjs/seo"],
+  modules: ["nuxt-schema-org", "@nuxtjs/seo", '@element-plus/nuxt',],
   site: {
     url: "https://dhwn9w-3000.csb.app/",
     name: "My Website",
---
+++ pages/affix.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+definePageMeta({ layout: "ssr" });
+</script>
+
+<template>
+    <div class="main">
+        <el-scroller>
+            <div class="scroll-container">
+                <div class="affix-container">
+                    <el-affix target=".scroll-container" :offset="200">
+                        <el-button type="primary">Target container</el-button>
+                    </el-affix>
+                </div>
+            </div>
+        </el-scroller>
+    </div>
+</template>
+<style>
+* {
+    margin: 0;
+    padding: 0;
+}
+
+.main {
+    text-align: center;
+    height: 100vh;
+    border-radius: 4px;
+    background: red;
+}
+
+.scroll-container {
+    height: 2800px;
+    padding: 0 16px;
+    background-color: aquamarine;
+}
+
+.affix-container {
+    text-align: center;
+    height: 100vh;
+    border-radius: 4px;
+    background: var(--el-color-primary-light-9);
+}
+</style>
